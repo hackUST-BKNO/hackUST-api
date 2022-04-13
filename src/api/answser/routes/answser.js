d4372e3c40deda8817bd548f5b7906495cc6d434
NavyNavi <kcchangrace@gmail.com>
2022-04-14 02:30:47 +0800

    Add collections

---
+++ src/api/answser/routes/answser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * answser router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::answser.answser');
---
+++ src/api/answser/services/answser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * answser service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::answser.answser');
