f2b0a143ff8becdc6228369ddfe1e08c63c2353a
NavyNavi <kcchangrace@gmail.com>
2022-04-14 17:28:53 +0800

    Add meilisearch plugin

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+
+module.exports = () => ({
+    //...
+    meilisearch: {
+      config: {
+        // Your meili host
+        host: "http://localhost:7700",
+        // Your master key or private key
+        apiKey: "CZlF1EhKOwSXnOqqVZFF",
+      }
+    }
+  })
